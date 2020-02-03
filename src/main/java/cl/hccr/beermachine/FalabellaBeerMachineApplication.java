cb7ea97069f23f2e9d0e7ee27c5cb6fec662e14c
Harold Campuzano <harold.campuzano@safecar.cl>
2020-02-03 13:14:31 -0300

    Initial Commit

---
+++ src/main/java/cl/hccr/beermachine/FalabellaBeerMachineApplication.java
@@ -0,0 +1,13 @@
+package cl.hccr.beermachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalabellaBeerMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FalabellaBeerMachineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/hccr/beermachine/FalabellaBeerMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.hccr.beermachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FalabellaBeerMachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
